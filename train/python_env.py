7c7c82df0c31372c8fd3493aceae91f83100e6d3
James <70284@sangfor.com>
2018-04-14 17:51:36 +0800

    Add train/python_env.py

---
+++ train/python_env.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Date    : 2018-04-12 08:52:07
+@Author  : Ethan (euler52201044@163.com)
+@Link    : https://github.com/Ethan16
+@file    : python_env.py
+@usage   : 
+@Version : 1.0
+@Change  : 2018-04-12 08:52:07
+"""
+
+
+import os
+
+
+# print os.getpid()
+# print os.umask(777)
+# print os.environ
+# print os.times()
+# print os.strerror(1)
+
+# print type(os)
+# print dir(os)
+print os.curdir
+# print os.system("dir")
