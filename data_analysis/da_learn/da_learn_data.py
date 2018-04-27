bda431dd9d7c3066a3ebb7417dda6e3b42d8e5fe
James <70284@sangfor.com>
2018-04-27 10:27:45 +0800

    [update]da_learn_data.py

--- data_analysis/da_learn/da_learn_data.py
+++ data_analysis/da_learn/da_learn_data.py
@@ -10,4 +10,22 @@
 @Change  : 2018-01-12 13:30:53
 """
 
+import numpy
 
+
+a = numpy.array([1, 2, 3])
+
+# print type(a)
+
+# print a.shape
+
+a = a.reshape((1, -1))
+
+# print a.shape
+
+a = numpy.array([1, 2, 3, 4, 5, 6])
+
+a = a.reshape((2, -1))
+
+print a.shape
+print a
